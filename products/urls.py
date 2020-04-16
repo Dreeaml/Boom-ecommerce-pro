bc264954f1df4175006cb7df64e176ca1635352d
Andrea Maggino <andreamaggino99@gmail.com>
2020-04-16 22:43:45 +0000

    Implement category filter for products and create a different view for products in home page

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
-from .views import all_products, product_detail
+from .views import home_products, all_products, product_detail
 
 urlpatterns = [
-    url(r'^$', all_products,  name="products"),
+    url(r'^$', home_products,  name="products"),
+    url(r'^all_products/$', all_products,  name="all_products"),
     url(r'^(\d+)$', product_detail, name='product_detail'),
-    #url(r'^<category>/$', all_products, name='all_products'),
-    #path('<category>/', all_products, name='all_products'),
-    #url(r'^(?P<category>[^\.]+)/$', all_products, name='all_products'),
+    url(r'^(?P<category>[\w-]+)/$', all_products, name='all_products'),
 ]
