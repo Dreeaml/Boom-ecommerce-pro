ad2efb98856e6ecc61028ef208c5e94f6a62a7eb
Andrea Maggino <andreamaggino99@gmail.com>
2020-04-29 07:19:56 +0000

    including import env in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+
 import os
 import dj_database_url
+if os.path.exists('env.py'):
+    import env
 
 
 
